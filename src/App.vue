fd8e5519e712f31b2b1b31f4917939f6440f20bd
Uheeking <wert1847@naver.com>
2022-12-21 22:04:30 +0900

    기초 작업

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">React</router-link> |
+    <router-link to="/about">Vue</router-link> |
+    <router-link to="/about">Vue</router-link>
   </nav>
   <router-view/>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" class="image_uheeking" src="https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2022%2F10%2Farmy-officer-recognized-for-rescuing-three-people-from-riptide-that-killed-yu-gi-oh-creator-ft.jpg?fit=max&cbr=1&q=90&w=750&h=500">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +16,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+.image_uheeking {
+  width: 300px;
+}
+</style>
