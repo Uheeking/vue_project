2a77ec268a54a56a72fd83938d41935336c5a8e6
Uheeking <wert1847@naver.com>
2022-12-22 22:56:02 +0900

    시간 관련 컴포넌트 생성

---
+++ src/components/AppClock.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h2 id="clock">{{ time }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  el: '#app',
+  data() {
+    return {
+      time: ''
+    }
+  },
+  methods: {
+    getClock() {
+      const date = new Date()
+      this.time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    }
+  },
+  mounted() {
+    setInterval(this.getClock, 1000) // 1초마다 시간 갱신
+  }
+}
+</script>
+
+<style scoped>
+#clock{
+    color: white
+}
+</style>
