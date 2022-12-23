3ab4501d3aea02db6c7f625d680ea295e6e0473b
Uheeking <wert1847@naver.com>
2022-12-23 15:59:53 +0900

    icon 관련 모듈 설치 및 전역으로 설정

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  fas,
+  faBed,
+  faCalendarDay,
+  faMoon,
+  faUtensils
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas, faBed, faCalendarDay, faMoon, faUtensils)
+
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(store)
+  .use(router)
+  .mount('#app')
