24b9190c01dceb2262adcbbb8d6aaca52fb46e64
Uheeking <wert1847@naver.com>
2022-12-26 21:34:00 +0900

    git pull 나는 에러 수정

--- src/main.js
+++ src/main.js
@@ -2,20 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  fas,
-  faBed,
-  faCalendarDay,
-  faMoon,
-  faUtensils
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(fas, faBed, faCalendarDay, faMoon, faUtensils)
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
   .use(store)
   .use(router)
   .mount('#app')
