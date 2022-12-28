8fba11bbf9bbcb0bbf95f8720c5366d8fb26fb45
Uheeking <wert1847@naver.com>
2022-12-28 12:13:11 +0900

    사이드바 생성

---
+++ src/components/About/SideBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="icon-bar">
+    <router-link to="/"><i class="fa fa-home"></i></router-link>
+    <router-link to="/main"
+      ><i class="fas fa-regular fa-calendar"></i
+    ></router-link>
+    <router-link to="/about"><i class="fas fa-question"></i></router-link>
+  </div>
+</template>
+
+<style>
+.icon-bar {
+  height: 100%;
+  width: 90px;
+  background-color: #555;
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  padding-top: 0;
+}
+
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 16px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 36px;
+}
+
+.icon-bar a:hover {
+  background-color: #858585;
+}
+
+.active {
+  background-color: #5eaf13;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>everything</div>
+    <SideBar />
   </div>
 </template>
+<script>
+import SideBar from '@/components/About/SideBar.vue'
+export default {
+  name: 'AboutView',
+  components: {
+    SideBar
+  }
+}
+</script>
